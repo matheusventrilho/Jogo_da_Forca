cb91f0edfef62d976a4da037253333db58a5aa3e
matheusventrilho <MatheusVentrilho@MacBook-Pro-de-Matheus.local>
2015-03-26 09:33:10 -0300

    Simplificando, arrumando e adicionando informações ao código

--- Jogo_da_Forca.py
+++ Jogo_da_Forca.py
@@ -3,18 +3,12 @@
 import turtle          # Usa a biblioteca de turtle graphics
 import random
 
+m = 0
+
 window = turtle.Screen()
 
-arquivo = open("entrada.txt","r", encoding="UTF-8")
-leitura = arquivo.readlines()
-lista_de_palavras = []
-
-for i in range(0,len(leitura)):
-    limpa = leitura[i].strip()
-    if (limpa != ""):
-        lista_de_palavras.append(limpa)
-        
-print(lista_de_palavras)
+#Escolhe uma palavra aleatória da lista importada para ser a palavra secreta
+lista_de_palavras = ["casa"]
 
 palavra = random.choice(lista_de_palavras)
 contagem_de_letras = len(palavra)
@@ -24,6 +18,7 @@ window.bgcolor("lightblue")
 window.title("Jogo da forca")
 
 
+#Define a cabeça
 def cabeca():
     cabeca = turtle.Turtle()
     cabeca.color("black")
@@ -32,7 +27,9 @@ def cabeca():
     cabeca.setpos(-200,130)
     cabeca.pendown()
     cabeca.circle(20)
+    cabeca.hideturtle()
 
+#Define o corpo
 def corpo():
     corpo = turtle.Turtle()
     corpo.color ("black")
@@ -42,7 +39,9 @@ def corpo():
     corpo.pendown()
     corpo.right(90)
     corpo.forward(80)
+    corpo.hidturtle()
 
+#Define a perna esquerda
 def perna_esquerda():
     perna_esquerda = turtle.Turtle()
     perna_esquerda.color ("black")
@@ -52,7 +51,9 @@ def perna_esquerda():
     perna_esquerda.pendown()
     perna_esquerda.left(225)
     perna_esquerda.forward(60)
+    perna_esquerda.hidturtle()
     
+#Define o braço esquerdo
 def braco_esquerdo():
     braco_esquerdo = turtle.Turtle()
     braco_esquerdo.color ("black")
@@ -62,7 +63,9 @@ def braco_esquerdo():
     braco_esquerdo.pendown()
     braco_esquerdo.left(225)
     braco_esquerdo.forward(60)
+    braco_esquerdo.hideturtle()
     
+#Define a perna direita
 def perna_direita():
     perna_direita = turtle.Turtle()
     perna_direita.color ("black")
@@ -72,7 +75,9 @@ def perna_direita():
     perna_direita.pendown()
     perna_direita.right(45)
     perna_direita.forward(60)
+    perna_direita.hideturtle()
     
+#Define o braço direito
 def braco_direito():
     braco_direito = turtle.Turtle()
     braco_direito.color ("black")
@@ -82,17 +87,12 @@ def braco_direito():
     braco_direito.pendown()
     braco_direito.right(45)
     braco_direito.forward(60)
+    braco_direito.hideturtle()
 
-def espacos():
-    espacos = turtle.Turtle()
-    espacos.color("black")
-    espacos.speed(5)
-    espacos.penup()
-    espacos.setpos(-150,-180)
-    espacos.forward(30)
-    espacos.penup()
+i = 0
+c = 0
     
-    #Define a base:
+#Define a base da forca
 forca = turtle.Turtle()
 forca.color("black")
 forca.speed(5)
@@ -106,22 +106,70 @@ forca.forward(100)
 forca.right(90)
 forca.forward (30)
 forca.penup()
+forca.left(90)
+forca.hideturtle
  
-espacos()*contagem_de_letras
- 
+#Define a quantidade de epsaços que serão a base para cada letra da palavra secreta
+while i < contagem_de_letras:
+    tracos = turtle.Turtle()
+    tracos.penup()
+    tracos.setpos(-150+c,-200)
+    tracos.pendown()
+    tracos.forward(20)
+    i +=1
+    c += 30
+    tracos.hideturtle()
+    
+
 letras = []
 
-letra = window.textinput("Letra", "Tente acertar uma letra da palavra secreta")
-letras.append(letra)
-
-partes_do_humano = [cabeca(),corpo(),braco_direito(),braco_esquerdo(),perna_direita(),perna_esquerda()]
-
 erro = 0
+
+t = 0
+
+while erro < 6:
+    letra = window.textinput("Letra", "Chute uma letra")
+    letras.append(letra)
+
+
+    for letra in letras:
+     #   window.write ("Essa letra já foi utilizada")
+        t = 0
+
+
+    else:
+        for letra in palavra:
+            pass
+            
         
-def erros():
-    partes_do_humano[0]
-    del partes_do_humano[0]
+        else: 
+            erro += 1
+        
+            if erro == 1:
+                cabeca()
+    
+            elif erro == 2:
+                corpo()
+        
+            elif erro == 3:
+                braco_direito()
+        
+            elif erro == 4:
+                braco_esquerdo()
+        
+            elif erro == 5:
+                perna_direita()
+    
+        
+if erro == 6:
+    perna_esquerda()
+    
+   # window.write ("Game over!")
+    m = window.textinput("Jogo da forca", "Para jogar novamente digite 0")
+
     
 
 
+
 window.exitonclick()
+
