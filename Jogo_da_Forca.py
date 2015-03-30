9626292f97f272f7c31621386e1a0c02fca62cf8
matheusventrilho <MatheusVentrilho@MacBook-Pro-de-Matheus.local>
2015-03-30 09:21:01 -0300

    Troca de variáveis para utilizar a habilidade "reset"

--- Jogo_da_Forca.py
+++ Jogo_da_Forca.py
@@ -14,95 +14,12 @@ window = turtle.Screen()
 lista_de_palavras = ["São Paulo"]
 
 palavra = random.choice(lista_de_palavras).upper()
+#lista_de_palavras.remove(palavra)
 contagem_de_letras = len(palavra)
   
 #Cria uma janela
 window.bgcolor("lightblue")
 window.title("Jogo da forca")
-
-
-#Define a cabeça
-def cabeca():
-    cabeca = turtle.Turtle()
-    cabeca.color("black")
-    cabeca.speed(5)
-    cabeca.penup()       
-    cabeca.setpos(-200,130)
-    cabeca.pendown()
-    cabeca.circle(20)
-    cabeca.hideturtle()
-
-#Define o corpo
-def corpo():
-    corpo = turtle.Turtle()
-    corpo.color ("black")
-    corpo.speed(5)
-    corpo.penup()
-    corpo.setpos(-200,130)
-    corpo.pendown()
-    corpo.right(90)
-    corpo.forward(80)
-    corpo.hideturtle()
-
-#Define a perna esquerda
-def perna_esquerda():
-    perna_esquerda = turtle.Turtle()
-    perna_esquerda.color ("black")
-    perna_esquerda.speed(5)
-    perna_esquerda.penup()
-    perna_esquerda.setpos(-200,50)
-    perna_esquerda.pendown()
-    perna_esquerda.left(225)
-    perna_esquerda.forward(60)
-    perna_esquerda.hideturtle()
-    
-#Define o braço esquerdo
-def braco_esquerdo():
-    braco_esquerdo = turtle.Turtle()
-    braco_esquerdo.color ("black")
-    braco_esquerdo.speed(5)
-    braco_esquerdo.penup()
-    braco_esquerdo.setpos(-200,110)
-    braco_esquerdo.pendown()
-    braco_esquerdo.left(225)
-    braco_esquerdo.forward(60)
-    braco_esquerdo.hideturtle()
-    
-#Define a perna direita
-def perna_direita():
-    perna_direita = turtle.Turtle()
-    perna_direita.color ("black")
-    perna_direita.speed(5)
-    perna_direita.penup()
-    perna_direita.setpos(-200,50)
-    perna_direita.pendown()
-    perna_direita.right(45)
-    perna_direita.forward(60)
-    perna_direita.hideturtle()
-    
-#Define o braço direito
-def braco_direito():
-    braco_direito = turtle.Turtle()
-    braco_direito.color ("black")
-    braco_direito.speed(5)
-    braco_direito.penup()
-    braco_direito.setpos(-200,110)
-    braco_direito.pendown()
-    braco_direito.right(45)
-    braco_direito.forward(60)
-    braco_direito.hideturtle()
-    
-def acentos(txt):
-    return normalize ("NFKD",txt).encode("ASCII","ignore").decode("ASCII")
-if __name__ == "__main__":
-    from doctest import testmod
-    testmod()
-    
-palavra2 = acentos(palavra)
-        
-
-c = 0
-x = 0
 
 #Define a base da forca
 forca = turtle.Turtle()
@@ -123,26 +40,105 @@ forca.left(90)
 
 
 
+#Define a cabeça
+def cabeca():
+    forca.color("black")
+    forca.speed(5)
+    forca.penup()       
+    forca.setpos(-200,130)
+    forca.pendown()
+    forca.circle(20)
+    forca.hideturtle()
+
+#Define o corpo
+def corpo():
+    forca.color ("black")
+    forca.speed(5)
+    forca.penup()
+    forca.setpos(-200,130)
+    forca.pendown()
+    forca.right(90)
+    forca.forward(80)
+    forca.hideturtle()
+
+#Define a perna esquerda
+def perna_esquerda():
+    forca.color ("black")
+    forca.speed(5)
+    forca.penup()
+    forca.setpos(-200,50)
+    forca.pendown()
+    forca.left(90)
+    forca.forward(60)
+    forca.hideturtle()
+    
+#Define o braço esquerdo
+def braco_esquerdo():
+    forca.color ("black")
+    forca.speed(5)
+    forca.penup()
+    forca.setpos(-200,110)
+    forca.pendown()
+    forca.left(90)
+    forca.forward(60)
+    forca.hideturtle()
+    
+#Define a perna direita
+def perna_direita():
+    forca.color ("black")
+    forca.speed(5)
+    forca.penup()
+    forca.setpos(-200,50)
+    forca.pendown()
+    forca.right(90)
+    forca.forward(60)
+    forca.hideturtle()
+    
+#Define o braço direito
+def braco_direito():
+    forca.color ("black")
+    forca.speed(5)
+    forca.penup()
+    forca.setpos(-200,110)
+    forca.pendown()
+    forca.right(45)
+    forca.forward(60)
+    forca.hideturtle()
+    
+def acentos(txt):
+    return normalize ("NFKD",txt).encode("ASCII","ignore").decode("ASCII")
+if __name__ == "__main__":
+    from doctest import testmod
+    testmod()
+    
+palavra2 = acentos(palavra)
+        
+
+c = 0
+x = 0
+
+
+
+
+
 while m <= 1:
     
 #Define a quantidade de epsaços que serão a base para cada letra da palavra secreta
     for x in palavra:
         if x == " ":
-            tracos = turtle.Turtle()
-            tracos.penup()
-            tracos.setpos(-150+c,-200)
-            tracos.forward(20)
+            forca.penup()
+            forca.setpos(-150+c,-200)
+            forca.forward(20)
             c += 30
-            tracos.hideturtle()
+            forca.hideturtle()
         
         if x != " ":
-            tracos = turtle.Turtle()
-            tracos.penup()
-            tracos.setpos(-150+c,-200)
-            tracos.pendown()
-            tracos.forward(20)
+            forca.penup()
+            forca.setpos(-150+c,-200)
+            forca.pendown()
+            forca.forward(20)
             c += 30
-            tracos.hideturtle()
+            forca.hideturtle()
             
     
 
@@ -191,7 +187,7 @@ while m <= 1:
                     forca.penup()
                     forca.setpos(-150+30*j, -200)
                     forca.pendown()
-                    forca.write(palavra[j], font = ("Arial",12))
+                    forca.write(palavra[j], font = ("Arial",16))
                     forca.hideturtle()
                     j += 1
                     acerto += 1
@@ -205,11 +201,17 @@ while m <= 1:
            erros = turtle.Turtle()
            erros.write ("Game over!", font = ("Arial",20))
            erros.hideturtle()
-           time.sleep(2)
+           time.sleep(1.5)
+           erros.clear()
            jogar_novamente = window.textinput("Jogo da forca", "Você deseja jogar novamente")
     
            if jogar_novamente == "sim":
-               m += 0
+               m -= 1
+               erro = 0
+               acerto = 0
+               forca.reset()
+               
+               
            if jogar_novamente == "nao":
                m += 1
         
@@ -217,11 +219,16 @@ while m <= 1:
             acertos = turtle.Turtle()
             acertos.write ("Parabéns, você ganhou!",font = ("Arial",20))
             acertos.hideturtle()
-            time.sleep(2)
+            time.sleep(1.5)
+            acertos.clear()
             jogar_novamente = window.textinput("Jogo da forca", "Você deseja jogar novamente")
     
             if jogar_novamente == "sim":
-               m += 0
+               m -= 1
+               erro = 0
+               acerto = 0
+               forca.reset()
+               
             if jogar_novamente == "nao":
                m += 1
 
